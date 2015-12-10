fd94ad6cee1a2c48e8e17406dd6217235ed80058
OfficeGemini <slagria@officegemini.com>
2015-12-10 11:37:12 -0600

    Adding solution

---
+++ Interview/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interview
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("All documents:");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Documents over 200 Size:");
+   
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Documents over 200 Size and is a PDF:");
+
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("All Color Documents:");
+
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("All PdfVersion = 2 Documents:");
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ InterviewLibrary/Document.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interview
+{
+
+    public abstract class Document
+    {
+
+        public string Name { get; set; }
+
+        public int Size { get; set; }
+
+        public abstract string Display();
+    }
+
+    public enum PixelType
+    {
+        Color,
+        GrayScale,
+        BlackAndWhite
+    }
+
+    public interface IDocumentProvider
+    {
+        IEnumerable<Document> GetDocuments();
+    }
+
+}
